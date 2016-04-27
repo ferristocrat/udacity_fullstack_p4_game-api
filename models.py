d50b8384df14c7cf984f4dee4d7d9dc9ea727ffb
ferristocrat <michael@ferristocrat.com>
2016-04-26 22:11:17 -0400

    refactor: separating api from models and front end
    Built out API endpoint logic that is in a separate file than the views

---
+++ models.py
@@ -0,0 +1,129 @@
+"""models.py - This file contains the class definitions for the Datastore
+entities used by the Game. Because these classes are also regular Python
+classes they can include methods (such as 'to_form' and 'new_game')."""
+
+import urllib
+import random
+from datetime import date
+from protorpc import messages
+from google.appengine.ext import ndb
+
+
+class User(ndb.Model):
+    """User profile"""
+    name = ndb.StringProperty(required=True)
+    email = ndb.StringProperty()
+
+
+class Game(ndb.Model):
+    """Game object"""
+    word_length = ndb.IntegerProperty(required=True)
+    word = ndb.StringProperty(required=True)
+    attempts_allowed = ndb.IntegerProperty(required=True)
+    attempts_remaining = ndb.IntegerProperty(required=True, default=5)
+    game_over = ndb.BooleanProperty(required=True, default=False)
+    letters_guessed_correct = ndb.StringProperty(repeated=True)
+    letters_guessed_wrong = ndb.StringProperty(repeated=True)
+    user = ndb.KeyProperty(required=True, kind='User')
+
+    def add_correct_guess(self, value):
+        """Adds the guess to letters_guessed_correct"""
+        self.letters_guessed_correct.append(value)
+        self.put()
+
+    def add_wrong_guess(self, value):
+        """Adds the guess to letters_guessed_wrong"""
+        self.letters_guessed_wrong.append(value)
+        self.put()
+
+    @classmethod
+    def new_game(cls, user, word_length):
+        """Creates and returns a new game"""
+        if word_length < 10 or word_length > 20:
+            raise ValueError('Word must be between 10 and 20.')
+        random_word_url = "http://randomword.setgetgo.com/get.php?len=" + \
+            str(word_length)
+        word = urllib.urlopen(random_word_url).read().upper()
+        game = Game(user=user,
+                    word=word,
+                    word_length=word_length,
+                    attempts_allowed=6,
+                    attempts_remaining=6,
+                    game_over=False)
+        game.put()
+        return game
+
+    def to_form(self, message):
+        """Returns a GameForm representation of the Game"""
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.user_name = self.user.get().name
+        form.attempts_remaining = self.attempts_remaining
+        form.game_over = self.game_over
+        form.message = message
+        return form
+
+    def end_game(self, won=False):
+        """Ends the game - if won is True, the player won. - if won is False,
+        the player lost."""
+        self.game_over = True
+        self.put()
+        # Add the game to the score 'board'
+        score = Score(user=self.user, date=date.today(), won=won,
+                      guesses=self.attempts_allowed - self.attempts_remaining)
+        score.put()
+
+
+class Score(ndb.Model):
+    """Score object"""
+    user = ndb.KeyProperty(required=True, kind='User')
+    date = ndb.DateProperty(required=True)
+    won = ndb.BooleanProperty(required=True)
+    guesses = ndb.IntegerProperty(required=True)
+
+    def to_form(self):
+        return ScoreForm(user_name=self.user.get().name, won=self.won,
+                         date=str(self.date), guesses=self.guesses)
+
+
+class GameForm(messages.Message):
+    """GameForm for outbound game state information"""
+    urlsafe_key = messages.StringField(1, required=True)
+    attempts_remaining = messages.IntegerField(2, required=True)
+    game_over = messages.BooleanField(3, required=True)
+    message = messages.StringField(4, required=True)
+    user_name = messages.StringField(5, required=True)
+
+
+class GameForms(messages.Message):
+    """Return multiple ScoreForms"""
+    items = messages.MessageField(GameForm, 1, repeated=True)
+
+
+class NewGameForm(messages.Message):
+    """Used to create a new game"""
+    user_name = messages.StringField(1, required=True)
+    word_length = messages.IntegerField(2, default=10)
+
+
+class MakeMoveForm(messages.Message):
+    """Used to make a move in an existing game"""
+    guess = messages.StringField(1, required=True)
+
+
+class ScoreForm(messages.Message):
+    """ScoreForm for outbound Score information"""
+    user_name = messages.StringField(1, required=True)
+    date = messages.StringField(2, required=True)
+    won = messages.BooleanField(3, required=True)
+    guesses = messages.IntegerField(4, required=True)
+
+
+class ScoreForms(messages.Message):
+    """Return multiple ScoreForms"""
+    items = messages.MessageField(ScoreForm, 1, repeated=True)
+
+
+class StringMessage(messages.Message):
+    """StringMessage-- outbound (single) string message"""
+    message = messages.StringField(1, required=True)
